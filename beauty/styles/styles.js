5f8868741ce13eacf56573ff26b96644285e6291
Roberto <san20086@byui.edu>
2023-09-26 19:53:45 -0600

    Overlay effect trasition fix.

---
+++ beauty/styles/styles.js
@@ -0,0 +1,4 @@
+//This function belongs to the Overlay Hero effect ".active" in css//
+function toggleOverlay() 
+    const overlay = document.getElementById("myOverlay");
+    overlay.classList.toggle("active");
